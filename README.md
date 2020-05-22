9a02991d13ee451ffc9ec25e74cc3a7a72f3f6b0
Daniel William Clarke <DClarke5@slb.com>
2020-05-22 15:43:37 +0100

    fix comment, add readme

---
+++ README.md
@@ -0,0 +1,12 @@
+# Semi Autonomous Finite State Machines
+
+## Final year project source code
+
+### Stored here for posterity
+
+Written in C++,
+Generates semi autonomous finite state machines based on a complex regex input
+This software is meant to interface with an OPCServer and provide control commands from the FSM based on the input from it.
+See something like KepServerEx5
+
+Ui is implemented using SFML from 2013 (would need some work to update to use the newer framework)
--- src/nfa.h
+++ src/nfa.h
@@ -1,8 +1,8 @@
-/ This code is in the public domain - feel free to do anything you 
+// This code is in the public domain - feel free to do anything you
 // wish with it.
 //
 // Eli Bendersky (spur4444@yahoo.com)
-// 
+//
 
 #ifndef NFA_H
 #define NFA_H
@@ -17,30 +17,32 @@
 // must create a vector of states
 //typedef unsigned state;
 
-// the input must become a map object 
-// must include for timing 
+// the input must become a map object
+// must include for timing
 //typedef char input;
 //<channel><opcode><value>
 //typedef Trans_Input* input;
 typedef char input;
-typedef std::vector<State*>::iterator StateIter;
-typedef std::vector<State*> stateVec;
-enum {EPS = -1, NONE = 0};
-
+typedef std::vector<State *>::iterator StateIter;
+typedef std::vector<State *> stateVec;
+enum
+{
+	EPS = -1,
+	NONE = 0
+};
 
 class NFA
 {
 public:
-    
 	NFA();
 
-	NFA& operator=(const NFA& other);
+	NFA &operator=(const NFA &other);
 
-	void fill_states(const NFA& otherNFA);
+	void fill_states(const NFA &otherNFA);
 
 	void setNewInitial();
-			
-    void show(void);
+
+	void show(void);
 
 	stateVec getInitialState();
 	stateVec getFinalState();
@@ -54,26 +56,26 @@ public:
 	void setSize(unsigned);
 	void setType(std::string);
 
-	void addInitalState(State*);
-	void addFinalState(State*);
-	void addStateSet(State*);
+	void addInitalState(State *);
+	void addFinalState(State *);
+	void addStateSet(State *);
 
 private:
 	std::string type;
 	//sets of initial and final states
-    std::vector<State*> initialState;
-    std::vector<State*> finalState;
+	std::vector<State *> initialState;
+	std::vector<State *> finalState;
 	//a set of all states
-	std::vector<State*> stateSet;
+	std::vector<State *> stateSet;
 	// total number of states in the nfa
-    unsigned size;
+	unsigned size;
 };
 
 // NFA building functions
 //
-NFA* build_nfa_basic(input in);
-NFA* build_nfa_alter(NFA* nfa1, NFA* nfa2);
-NFA* build_nfa_concat(NFA* nfa1, NFA* nfa2);
-NFA* build_nfa_star(NFA* nfa);
+NFA *build_nfa_basic(input in);
+NFA *build_nfa_alter(NFA *nfa1, NFA *nfa2);
+NFA *build_nfa_concat(NFA *nfa1, NFA *nfa2);
+NFA *build_nfa_star(NFA *nfa);
 
 #endif // NFA_H
